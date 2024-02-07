8c0348a53841518fe500b74fa6cc8a97e95fd368
AR Aourangazeb Alif <ar.aourangazeb.alif@gmail.com>
2024-02-07 20:52:49 +0600

    solved the third problem

--- problem.js
+++ problem.js
@@ -9,24 +9,44 @@ function calculateMoney(ticketSale) {
             const roi = dailyIncome - dailySpend;
             return roi;
 
-        }else{
+        } else {
             return "!!!Invalid Number!!!\nTicket sale can't be negative value...Give a positive value : )";
         }
-    }else{
+    } else {
         return "!!!Invalid input!!!\nGive a number value : )";
     }
 }
 
 // Problem - 2 
 function checkName(name) {
-    const expectedLastChars = [`a`,`y`,`i`,`e`,`o`,`u`,`w`];
-    if(typeof name === 'string'){
-        if(expectedLastChars.includes(name[name.length-1].toLowerCase())){
+    const expectedLastChars = [`a`, `y`, `i`, `e`, `o`, `u`, `w`];
+    if (typeof name === 'string') {
+
+        if (expectedLastChars.includes(name[name.length - 1].toLowerCase())) {
             return "Good Name";
-        }else{
+        } else {
             return "Bad Name";
         }
-    }else{
+
+    } else {
         return "invalid";
+    }
+}
+
+// Problem - 3 
+function deleteInvalids(array) {
+    if (Array.isArray(array)) {
+
+        const outputArray = [];
+
+        for (const i of array) {
+            if (typeof i === 'number' && !isNaN(i)) {
+                outputArray.push(i);
+            }
+        }
+        return outputArray;
+
+    } else {
+        return "Invalid Array";
     }
 }
